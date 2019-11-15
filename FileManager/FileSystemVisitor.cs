5f2f4fd1cc8f2d79d50e38d7f130869fcb8868b7
George Eremin <e_goga2010@mail.ru>
2019-11-15 20:48:51 +0600

    Добавьте файлы проекта.

---
+++ FileManager/FileSystemVisitor.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace FileManager
+{
+    public class FileSystemVisitor
+    {
+        delegate void search();
+        string path;
+        string name;
+        List<string> names = new List<string>();
+
+        public FileSystemVisitor(string path)
+        {
+            this.path = path;
+            search search;
+            search = FindFilesAndDirectories;
+            search += ShowFileTree;
+            search();
+        }
+
+        public FileSystemVisitor(string path,string name)
+        {
+            this.path = path;
+            this.name = name;
+            search search;
+            search = FindFilesAndDirectories;
+            search += FilterByNameInDirectory;
+            search += ShowFileTree;
+            search();
+        }
+
+        public void ShowFileTree()
+        {
+            Console.WriteLine($"\r\nAll directories and files in {path}");
+            foreach (var name in names)
+            {                              
+                Console.WriteLine("\t" + name);
+            }
+            Console.WriteLine("The searching is finished.");
+        }
+
+        public void FindFilesAndDirectories()
+        {
+            Console.WriteLine("Started searching...");
+            if (Directory.Exists(path))
+            {
+                List<string> n = new List<string>();
+                names = Directory.GetDirectories(path).ToList();
+                names.AddRange(Directory.GetFiles(path).ToList());
+                foreach (var name in names)
+                {
+                    n.Add(name.Replace(path + "\\", ""));
+                }
+                names = n;
+            }
+            else
+            {
+                Console.WriteLine("Path is incorrect.");
+            }
+        }
+
+        public void FilterByNameInDirectory()
+        {
+            names = names.Where(n => n.Contains(name)).ToList();
+        }
+
+
+    }
+}
---
+++ FileManager/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileManager
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FileSystemVisitor fileSystemVisitor;
+            bool stop = false;
+            do
+            {
+                Console.Write("Enter command: ");
+                string command = Console.ReadLine();
+                string path;
+                switch (command) { 
+                    case "exit":
+                        stop = true;
+                        break;
+                    case "sf":
+                        Console.Write("Enter path: ");
+                        path = Console.ReadLine();
+                        fileSystemVisitor = new FileSystemVisitor(path);
+                        break;
+                    case "sfp":
+                        Console.Write("Enter parameters: ");
+                        Console.Write("\r\nEnter path: ");
+                        path = Console.ReadLine();
+                        Console.Write("\r\nEnter name: ");
+                        string name = Console.ReadLine();
+                        fileSystemVisitor = new FileSystemVisitor(path,name);
+                        break;
+                    default:
+                        Console.WriteLine($"Error. Command {command} does not exist.");
+                        break;
+                }
+                
+            }
+            while (stop == false);
+        }
+    }
+}
